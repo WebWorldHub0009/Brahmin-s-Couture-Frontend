5787e69060c090d96d173bdeac8841e164d5d9ef
Shuaib0111 <mohdshuaib77689@gmail.com>
2025-08-26 16:53:24 +0530

    26 aug agian

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -7,7 +7,7 @@ const api = axios.create({
   withCredentials: true,
 });
 
-// Attach token to every request automatically
+// Attach token to every request 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token"); // ✅ fixed this line
 
